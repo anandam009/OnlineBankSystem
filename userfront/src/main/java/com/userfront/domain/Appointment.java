62ddffa2623325779d0ba26f3ffc36c3cb202587
Vladimir Bilenko <vbilenko@yahoo.com>
2016-12-16 14:23:47 -0800

    finished domain models

---
+++ userfront/src/main/java/com/userfront/domain/Appointment.java
@@ -0,0 +1,55 @@
+package com.userfront.domain;
+
+import java.util.Date;
+
+/**
+ * Created by vbilenko on 12/16/16.
+ */
+public class Appointment {
+
+    private Long id;
+    private Date date;
+    private String location;
+    private String description;
+    private boolean confirmed;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+}
